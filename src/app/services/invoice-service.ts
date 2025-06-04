bebdf0ccc322b271300accfe66b3509b47a4035c
larry <larryking8118@gmail.com>
2025-06-04 11:52:04 +0000

    created invoice service and mock data

---
+++ src/app/services/invoice-service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { InvoiceInterface } from '../shared/invoiceInterface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvoiceService {
+
+  constructor() { }
+
+  createInvoice(invoice: InvoiceInterface) {
+    
+  }
+
+  fetchInvoices() {
+
+  }
+
+  updateInvoice() {
+
+  }
+
+  deleteInvoice(invoice: InvoiceInterface) {
+
+  }
+
+
+
+}
---
+++ src/app/shared/invoiceInterface.ts
@@ -0,0 +1,31 @@
+export enum InvoiceStatus {
+    paid = "Paid",
+    pending = "Pending",
+    draft = "Draft"
+}
+
+
+export interface InvoiceItem {
+    itemName: string,
+    quantity: string,
+    price: string,
+    total: string
+}
+
+
+export interface InvoiceInterface {
+    id: string,
+    invoiceName: string,
+    invoiceDate: string,
+    paymentDate: string,
+    clientName: string,
+    clientEmail: string,
+    status: InvoiceStatus,
+    items: InvoiceItem[],
+    clientCountry?: string,
+    clientCity?: string,
+    clientStreetName?: string,
+    clientStreetNumber?: string
+
+}
+
